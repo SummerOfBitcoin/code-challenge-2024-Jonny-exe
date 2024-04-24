76af04fa2bf74c6292f15f808dbe6679e959229d
laptop <160007447+Aikasle@users.noreply.github.com>
2024-04-25 01:08:20 +0200

    test

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ import time
 TARGET_HASH = "0000ffff00000000000000000000000000000000000000000000000000000000"
 TARGET_HASH_FORMATED = format_target(TARGET_HASH) 
 PREVIOUS_BLOCK = bytes.fromhex("000000000000000000015b32060fb2b834a4f799616500ab2af7277e93d70736")
-BLOCK_BITS = int("1f00ffff", 16).to_bytes(length=4, byteorder="little")
+BLOCK_BITS = int("1f00ffff", 16).to_bytes(length=4, byteorder="big")
+# BLOCK_BITS = int("1f00ffff", 16).to_bytes(length=4, byteorder="little")
 VERSION = (2).to_bytes(length=4, byteorder="little")
 
 def mine(target, merkletree):
@@ -107,6 +108,7 @@ if __name__ == "__main__":
 
 
     block_hash, block_header = mine(TARGET_HASH_FORMATED, merkletree)
+    print(block_hash.hex())
 
     coinbase_transaction_data = open_file_as_json("example.json")
     reward = calculate_block_reward(trans)
