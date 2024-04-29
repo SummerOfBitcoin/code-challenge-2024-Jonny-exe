458b194972ff0e7eca48953fc9a2bc5bf5bc4eb1
laptop <160007447+Aikasle@users.noreply.github.com>
2024-04-29 18:26:15 +0200

    update

--- write_raw.py
+++ write_raw.py
@@ -420,7 +420,7 @@ def test():
                 invalidcount += 1
             # if validcount + invalidcount > 100:
             #     break
-            if validcount > 100:
+            if validcount > 10000:
                 # print(filepath)
                 break
             if (validcount + invalidcount) % 100 == 0:
